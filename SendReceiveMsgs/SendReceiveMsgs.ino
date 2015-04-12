ddcd8b040dd53ff02056098a56d53268b7af9973
Veneciaaa <valvarez@iastate.edu>
2015-04-11 20:12:45 -0500

    Test to send and receive messages

---
+++ SendReceiveMsgs/SendReceiveMsgs.ino
@@ -0,0 +1,44 @@
+void setup() {
+  Serial.begin(9600);
+  
+   Ethernet.begin(mac, ip);
+   delay(1000);
+  Serial.println("connecting...");
+  
+   if (client.connect(server, 80)) {
+    Serial.println("connected");
+    // Make a HTTP request:
+    client.println("GET /search?q=arduino HTTP/1.1");
+    client.println("Host: www.google.com");
+    client.println("Connection: close");
+    client.println();
+  }
+  else {
+    // kf you didn't get a connection to the server:
+    Serial.println("connection failed");
+  }
+
+
+}
+
+void loop() {
+  
+  // if there are incoming bytes available
+  // from the server, read them and print them:
+  if (client.available()) {
+    char c = client.read();
+    Serial.print(c);
+  }
+
+  // if the server's disconnected, stop the client:
+  if (!client.connected()) {
+    Serial.println();
+    Serial.println("disconnecting.");
+    client.stop();
+    // do nothing forevermore:
+    while (true);
+  }
+  
+}
+
+
